19c74dc7889c41751dee791ab72dd1d294b88c39
Vladimir Pavković <vladimirpavk@gmail.com>
2019-11-13 13:44:00 +0100

    +

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
+import Aux from '../Hoc/Aux';
 import BurgerIngridient from './BurgerIngridients/BurgerIngridient';
 import classes from './Burger.module.css';
 
-const Burger = ()=>{
+const Burger = (props)=>{
+    //console.log(props.ingridients);
+    let burgerIngridients = [];
+    for(let ingridientName in props.ingridients){
+        for(let i=0; i<props.ingridients[ingridientName]; i++){
+            burgerIngridients.push(<BurgerIngridient key={Math.random()} type={ingridientName}/>)
+        }
+    }
+    console.log(burgerIngridients);
+    if(burgerIngridients.length === 0){
+        console.log('Empty ingridients');
+        burgerIngridients=<p>Please start adding ingridients...</p>
+    }
+
     return(
         <div className={classes.Burger}>
-            <BurgerIngridient type="bread-top" />            
-            <BurgerIngridient type="meat" />
-            <BurgerIngridient type="cheese" />
-            <BurgerIngridient type="bacon" />
-            <BurgerIngridient type="salad" />
+            <BurgerIngridient type="bread-top" />                        
+            {burgerIngridients}            
             <BurgerIngridient type="bread-bottom" />
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,25 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component{
+    constructor(props){
+        super(props);
+
+        //state i.e.
+        this.state = {
+            ingridients: {
+                "meat": 0,
+                "salad": 0,
+                "bacon": 0,
+                "cheese": 0
+            }     
+        };
+    }
+
     render() {
         return (
             <div>
                 <div>Burger</div>
-                <Burger />
+                <Burger ingridients={this.state.ingridients}/>
                 <div>Build controls</div>
             </div>
         );
